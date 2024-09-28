221a88bd49456e7326d2321091e39fa9ecab4974
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-28 16:44:45 +0900

    Separate Interfaces for Actions

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,15 +4,21 @@ interface RepositoriesState {
 	data: string[];
 }
 
-interface Action {
-	type: string;
-	payload?: any;
+interface SearchRepositoriesAction {
+	type: "search_repositories";
 }
 
-const reducer = (
-	state: RepositoriesState,
-	action: Action
-): RepositoriesState => {
+interface SearchRepositoriesSuccessAction {
+	type: "search_repositories_success";
+	payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+	type: "search_repositories_error";
+	payload: string;
+}
+
+const reducer = (state: RepositoriesState, action: any): RepositoriesState => {
 	switch (action.type) {
 		case "search_repositories":
 			return { loading: true, error: null, data: [] };

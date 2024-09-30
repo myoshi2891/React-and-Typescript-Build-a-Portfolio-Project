749bed9a0c8d6a757105b603d8b2914da2011eb6
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-30 22:36:19 +0900

    Passing Code to the IFrame

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
 	const ref = useRef<any>();
+	const iframe = useRef<any>();
 	const [input, setInput] = useState("");
 	const [code, setCode] = useState("");
 
@@ -36,13 +37,26 @@ const App = () => {
 			},
 		});
 
-		setCode(result.outputFiles[0].text);
+		// setCode(result.outputFiles[0].text);
+
+		iframe.current.contentWindow.postMessage(
+			result.outputFiles[0].text,
+			"*"
+		);
 	};
 
 	const html = /*html*/ `
-		<script>
-			${code}
-		</script>
+		<html>
+			<head></head>
+			<body>
+				<div id="root"></div>
+				<script>
+					window.addEventListener('message', (e) => {
+						eval(e.data)
+					}, false)
+				</script>
+			</body>
+		</html>
 	`;
 	return (
 		<div>
@@ -54,7 +68,7 @@ const App = () => {
 				<button onClick={onClick}>Submit</button>
 			</div>
 			<pre>{code}</pre>
-			<iframe sandbox="allow-scripts" srcDoc={html} />
+			<iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
 		</div>
 	);
 };

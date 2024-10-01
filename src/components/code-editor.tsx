cc2294e5bf5f10f522fbd24a8583b081ffd796ab
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-01 21:11:33 +0900

    A Few More Options

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -5,7 +5,16 @@ const CodeEditor = () => {
 		<MonacoEditor
 			language="javascript"
 			theme="dark"
-			options={{ wordWrap: "on" }}
+			options={{
+				wordWrap: "on",
+				minimap: { enabled: false },
+				showUnused: false,
+				folding: false,
+				lineNumbersMinChars: 3,
+				fontSize: 16,
+				scrollBeyondLastLine: false,
+				automaticLayout: true,
+			}}
 			height="500px"
 		/>
 	);

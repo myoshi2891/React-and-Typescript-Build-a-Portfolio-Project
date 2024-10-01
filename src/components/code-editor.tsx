5cb7f2f4f971717d4b4fcbc73d83b7635b130943
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-01 21:04:42 +0900

    Crazy Monaco Editor Type Defs

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,14 @@
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-	return <MonacoEditor language="javascript" height="500px" />;
+	return (
+		<MonacoEditor
+			language="javascript"
+			theme="dark"
+			options={{ wordWrap: "on" }}
+			height="500px"
+		/>
+	);
 };
 
 export default CodeEditor;

02fbaf098e658f0bc2d53fc52bbb1f1c3a7b68a7
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-10-06 12:29:16 +0900

    Action Creator Return Type Annotations

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -2,25 +2,25 @@ import { type } from "os";
 import { ActionType } from "../action-types";
 import { CellTypes } from "../cell";
 
-interface MoveCellAction {
+export interface MoveCellAction {
 	type: ActionType.MOVE_CELL;
 	payload: {
 		id: string;
 		direction: "up" | "down";
 	};
 }
-interface DeleteCellAction {
+export interface DeleteCellAction {
 	type: ActionType.DELETE_CELL;
 	payload: string;
 }
-interface InsertCellBeforeAction {
+export interface InsertCellBeforeAction {
 	type: ActionType.INSERT_CELL_BEFORE;
 	payload: {
 		id: string;
 		type: CellTypes;
 	};
 }
-interface UpdateCellAction {
+export interface UpdateCellAction {
 	type: ActionType.UPDATE_CELL;
 	payload: {
 		id: string;
